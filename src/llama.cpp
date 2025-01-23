c2624b2fd324ff98cc137397f5b0e1d22869cb58
Iwan Kawrakow <iwan.kawrakow@gmail.com>
2025-01-23 13:10:03 +0200

    Add Deepseek-R1-Distill pre-tokenizer

--- src/llama.cpp
+++ src/llama.cpp
@@ -5591,7 +5591,7 @@ static void llm_load_vocab(
                 vocab.type_pre = LLAMA_VOCAB_PRE_TYPE_COMMAND_R;
                 vocab.tokenizer_clean_spaces = false;
             } else if (
-                tokenizer_pre == "qwen2") {
+                tokenizer_pre == "qwen2" || tokenizer_pre == "deepseek-r1-qwen") {
                 vocab.type_pre = LLAMA_VOCAB_PRE_TYPE_QWEN2;
                 vocab.tokenizer_clean_spaces = false;
             } else if (
