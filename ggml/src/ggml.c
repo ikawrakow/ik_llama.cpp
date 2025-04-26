957308ca09d8796cfb45f344258ab6e634ceb98a
Iwan Kawrakow <iwan.kawrakow@gmail.com>
2025-04-26 10:08:37 +0300

    Fix division by zero bug

--- ggml/src/ggml.c
+++ ggml/src/ggml.c
@@ -21790,7 +21790,7 @@ struct ggml_cplan ggml_graph_plan(const struct ggml_cgraph * cgraph, int n_threa
                     const struct ggml_tensor * k = node->src[1];
                     if (q->ne[1] == 1 && q->ne[3] == 1 && q->ne[2]/k->ne[2] > 1 && n_tasks > 1 && k->ne[1]/32 > 1) {
                         if (k->ne[2] > 1) {
-                            int nk = 32 * (k->ne[2]*k->ne[1]/(32*n_tasks));
+                            int nk = MAX(1, 32 * (k->ne[2]*k->ne[1]/(32*n_tasks)));
                             int nstep_k = k->ne[2]*k->ne[1]/nk;
                             size_t result_size = (Dv + 16)*q->ne[2]/k->ne[2]*sizeof(float);
                             size_t size = nstep_k*result_size;
