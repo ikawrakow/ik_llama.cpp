8ffad187abbb93b74db8ef813b6fdceec80e02b0
Kawrakow <iwankawrakow@gmail.com>
2025-06-05 08:31:20 +0300

    MMQ implementation for IQ4_KS_R4 and IQ5_KS_R4 (#493)
    * MMQ for iq4_ks_r4
    
    * MMQ for iq5_ks_r4
    
    * Add forgotten file
    
    * Another forgotten file
    
    ---------
    
    Co-authored-by: Iwan Kawrakow <iwan.kawrakow@gmail.com>

--- ggml/src/ggml-cuda/common.cuh
+++ ggml/src/ggml-cuda/common.cuh
@@ -600,6 +600,13 @@ struct ggml_cuda_type_traits<GGML_TYPE_IQ4_KS> {
 };
 
 template<>
+struct ggml_cuda_type_traits<GGML_TYPE_IQ4_KS_R4> {
+    static constexpr int qk = QK_K;
+    static constexpr int qr = QR4_XS;
+    static constexpr int qi = QI4_XS;
+};
+
+template<>
 struct ggml_cuda_type_traits<GGML_TYPE_IQ4_KSS> {
     static constexpr int qk = QK_K;
     static constexpr int qr = QR4_XS;
@@ -615,6 +622,13 @@ struct ggml_cuda_type_traits<GGML_TYPE_IQ5_K> {
 
 template<>
 struct ggml_cuda_type_traits<GGML_TYPE_IQ5_KS> {
+    static constexpr int qk = QK_K;
+    static constexpr int qr = QR5_XS;
+    static constexpr int qi = QI5_XS;
+};
+
+template<>
+struct ggml_cuda_type_traits<GGML_TYPE_IQ5_KS_R4> {
     static constexpr int qk = QK_K;
     static constexpr int qr = QR5_XS;
     static constexpr int qi = QI5_XS;
