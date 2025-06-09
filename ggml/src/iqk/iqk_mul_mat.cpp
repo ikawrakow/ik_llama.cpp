1e7793d1faa4ec090731964e2de5366fc2a6e23d
Iwan Kawrakow <iwan.kawrakow@gmail.com>
2025-06-09 09:00:56 +0300

    New iq2_kt: AVX2 dequantize

--- ggml/src/iqk/iqk_mul_mat.cpp
+++ ggml/src/iqk/iqk_mul_mat.cpp
@@ -242,7 +242,7 @@ struct MulMat {
             case GGML_TYPE_IQ1_S  : return nrc_y >= 32 ? GGML_TYPE_Q8_0_R8 : type;
             case GGML_TYPE_Q4_K   : return nrc_y >= 32 ? GGML_TYPE_Q8_1    : type;
             case GGML_TYPE_Q5_K   : return nrc_y >= 32 ? GGML_TYPE_Q8_1    : type;
-            case GGML_TYPE_IQ2_KT : return nrc_y >= 32 ? GGML_TYPE_F32 : type;
+            case GGML_TYPE_IQ2_KT : return nrc_y >= 32 ? GGML_TYPE_Q8_0_R8 : type;
             case GGML_TYPE_IQ3_KT : return nrc_y >= 32 ? GGML_TYPE_F32 : type;
             case GGML_TYPE_IQ4_KT : return nrc_y >= 32 ? GGML_TYPE_Q8_0_R8 : type;
             default: break;
