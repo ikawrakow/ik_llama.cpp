a66490410a366a9605234b94d67f3d9b7b389140
Thireus ☠ <Thireus@users.noreply.github.com>
2025-06-24 09:24:45 +0100

    Update ggml.h
    Increase GGML_MAX_CONTEXTS

--- ggml/include/ggml.h
+++ ggml/include/ggml.h
@@ -234,7 +234,7 @@
 
 #define GGML_MAX_DIMS           4
 #define GGML_MAX_PARAMS         2048
-#define GGML_MAX_CONTEXTS       64
+#define GGML_MAX_CONTEXTS       2048 // Bump from 64 to allow loading split models that have more than 64 chunks - https://github.com/ggml-org/whisper.cpp/discussions/2520
 #define GGML_MAX_SRC            10
 #ifndef GGML_MAX_NAME
 #define GGML_MAX_NAME           64
