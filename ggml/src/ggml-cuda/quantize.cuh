6970ef925f5824c9eaa9dcce5d96482c90ac42fc
Kawrakow <iwankawrakow@gmail.com>
2025-07-07 07:23:12 +0200

    CUDA: small PP performance improvement for MoE models (#589)
    * Trying to implement quantized fmoe - not working yet
    
    * This works, but is slower than the non-working version
    
    * quantize_mmq_q8_1_id
    
    * Minor
    
    ---------
    
    Co-authored-by: Iwan Kawrakow <iwan.kawrakow@gmail.com>

--- ggml/src/ggml-cuda/quantize.cuh
+++ ggml/src/ggml-cuda/quantize.cuh
@@ -30,5 +30,9 @@ void quantize_mmq_q8_1_cuda(
     const float * x, void * vy, const int64_t kx0, const int64_t kx1, const int64_t channels, const int64_t kx0_padded,
     const ggml_type type_x, cudaStream_t stream);
 
+void quantize_mmq_q8_1_id_cuda(
+    const float * x, void * vy, const char * row_mapping, const int64_t kx0, const int64_t kx1, const int64_t kx0_padded,
+    const ggml_type type_x, cudaStream_t stream);
+
 // For now only applicable for tensors with ne[1] = 1, ne[3] = 1, and useful if ne[2] > 1
 void quantize_tensor_q8_1_cuda(const struct ggml_tensor * src, void * vy, const enum ggml_type type, cudaStream_t stream);
