6258a120d5d75cf54624f158a63f0d922088978a
Thireus ☠ <Thireus@users.noreply.github.com>
2025-07-29 20:32:13 +0100

    Update llama.cpp

--- src/llama.cpp
+++ src/llama.cpp
@@ -20455,7 +20455,9 @@ static void llama_model_quantize_internal(const std::string & fname_inp, const s
 
         // TODO: avoid hardcoded tensor names - use the TN_* constants
         if (name.find("attn_v.weight")   != std::string::npos ||
-            name.find("attn_qkv.weight") != std::string::npos) {
+            name.find("attn_qkv.weight") != std::string::npos ||
+            name.find("eh_proj.weight")   != std::string::npos ||
+            name.find("shared_head.head") != std::string::npos) {
             ++qs.n_attention_wv;
         } else if (name == LLM_TN(model.arch)(LLM_TENSOR_OUTPUT, "weight")) {
             qs.has_output = true;
