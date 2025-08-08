3466dbda401671f67ecbd38ff48bd2b5f073e7fd
Iwan Kawrakow <iwan.kawrakow@gmail.com>
2025-08-08 19:20:39 +0300

    maxfp4: CUDA dequantize

--- ggml/src/ggml-cuda.cu
+++ ggml/src/ggml-cuda.cu
@@ -3498,6 +3498,7 @@ GGML_CALL static bool ggml_backend_cuda_supports_op(ggml_backend_t backend, cons
                     case GGML_TYPE_IQ3_S:
                     case GGML_TYPE_IQ3_XXS:
                     case GGML_TYPE_IQ4_NL:
+                    case GGML_TYPE_MXFP4:
                     case GGML_TYPE_IQ4_XS:
                     case GGML_TYPE_IQ2_KL:
                     case GGML_TYPE_IQ3_KS:
