86e927bfe943b58b621eba16e5cd3bd7d663794b
Saood Karim <saood05@gmail.com>
2025-08-30 04:48:12 -0500

    minor

--- examples/server/utils.hpp
+++ examples/server/utils.hpp
@@ -339,8 +339,6 @@ static size_t common_part(const llama_context * ctx, const llama_model * model, 
     return token_idx;
 }
 
-
-
 static bool ends_with(const std::string & str, const std::string & suffix) {
     return str.size() >= suffix.size() && 0 == str.compare(str.size() - suffix.size(), suffix.size(), suffix);
 }
