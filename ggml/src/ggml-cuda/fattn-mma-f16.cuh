8b4208e78925c9bf2e53cd340b7f1b21d976c10b
Kawrakow <iwankawrakow@gmail.com>
2025-09-23 16:43:02 +0200

    Fix #772 (#790)
    Co-authored-by: Iwan Kawrakow <iwan.kawrakow@gmail.com>

--- ggml/src/ggml-cuda/fattn-mma-f16.cuh
+++ ggml/src/ggml-cuda/fattn-mma-f16.cuh
@@ -1408,7 +1408,7 @@ void launch_fattn_mma(
 
         //const bool use_stream_k = cc >= CC_ADA_LOVELACE || tiles_efficiency_percent < 75;
         //  On my RTX-4080 the above is slightly slower for PP. It would be useful to try and see what happens on Blackwell
-        const bool use_stream_k = tiles_efficiency_percent < 75;
+        const bool use_stream_k = tiles_efficiency_percent < 75 || Q->ne[1] > 2048;
 
         blocks_num.x = use_stream_k ? nblocks_stream_k : ntiles_total;
         blocks_num.y = 1;
