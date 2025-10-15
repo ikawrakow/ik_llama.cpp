5118036239569365c90986d3e259475f4195768f
Iwan Kawrakow <iwan.kawrakow@gmail.com>
2025-10-15 16:04:24 +0300

    Better argsort (CPU)

---
+++ ggml/src/iqk/iqk_cpu_ops.cpp
@@ -0,0 +1,52 @@
+#include "iqk_cpu_ops.h"
+#include "ggml.h"
+
+#include <cstdint>
+#include <vector>
+#include <algorithm>
+
+void iqk_grouped_top_k([[maybe_unused]] ggml_tensor * dst, [[maybe_unused]] int ith, [[maybe_unused]] int nth) {
+}
+
+void iqk_argsort(ggml_tensor * dst, int ith, int nth) {
+
+    auto src = dst->src[0];
+    GGML_ASSERT(dst->type == GGML_TYPE_I32);
+    GGML_ASSERT(src->type == GGML_TYPE_F32);
+
+    auto nrows = ggml_nrows(src);
+    auto npt   = (nrows + nth - 1)/nth;
+    auto first = npt*ith;
+    auto last  = std::min(first + npt, nrows);
+    if (last <= first) return;
+
+    auto order = (ggml_sort_order)dst->op_params[0];
+    int nk = dst->op_params[1];
+
+    int ne00 = src->ne[0];
+    thread_local std::vector<std::pair<float,int>> aux;
+    if ((int)aux.size() < ne00) aux.resize(ne00);
+    //std::vector<std::pair<float,int>> aux(ne00);
+
+    for (int ir = first; ir < last; ++ir) {
+        auto data = (const float *)((const char *)src->data + ir*src->nb[1]);
+        for (int j = 0; j < ne00; ++j) aux[j] = {data[j], j};
+        if (nk < ne00) {
+            if (order == GGML_SORT_ORDER_DESC) {
+                std::partial_sort(aux.begin(), aux.begin() + nk, aux.end(), std::greater<std::pair<float,int>>{});
+            } else {
+                std::partial_sort(aux.begin(), aux.begin() + nk, aux.end());
+            }
+        } else {
+            if (order == GGML_SORT_ORDER_DESC) {
+                std::sort(aux.begin(), aux.end(), std::greater<std::pair<float,int>>{});
+            } else {
+                std::sort(aux.begin(), aux.end());
+            }
+        }
+        auto y = (int32_t *)((char *)dst->data + ir*dst->nb[1]);
+        for (int j = 0; j < ne00; ++j) y[j] = aux[j].second;
+    }
+
+}
+
