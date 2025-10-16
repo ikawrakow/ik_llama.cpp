3b8cb3beebc5854ce66187840e7391374991410d
Iwan Kawrakow <iwan.kawrakow@gmail.com>
2025-10-16 09:22:37 +0300

    Cleanup

--- ggml/src/iqk/iqk_cpu_ops.cpp
+++ ggml/src/iqk/iqk_cpu_ops.cpp
@@ -7,11 +7,19 @@
 #include <cmath>
 
 namespace {
+// Playing around with group scores: use sum of probabilities in the group
 inline float group_score(int n_per_group, const float * data) {
     float sum = 0;
     for (int j = 0; j < n_per_group; ++j) sum += data[j];
     return sum;
 }
+// Playing around with group scores: use max of probabilities in the group
+inline float group_score_max(int n_per_group, const float * data) {
+    float max = data[0];
+    for (int j = 1; j < n_per_group; ++j) max = std::max(max, data[j]);
+    return max;
+}
+// Actual top-nk group score: sum of top-nk probabilities in the group
 inline float group_score(int n_per_group, int nk, const float * data, float * aux) {
     for (int j = 0; j < n_per_group; ++j) aux[j] = data[j];
     std::partial_sort(aux, aux + nk, aux + n_per_group, std::greater<float>{});
@@ -19,14 +27,15 @@ inline float group_score(int n_per_group, int nk, const float * data, float * au
     for (int j = 0; j < nk; ++j) sum += aux[j];
     return sum;
 }
-inline float group_score_max(int n_per_group, const float * data) {
-    float max = data[0];
-    for (int j = 1; j < n_per_group; ++j) max = std::max(max, data[j]);
-    return max;
+inline std::vector<std::pair<float,int>> & get_work_buffer(size_t size) {
+    thread_local std::vector<std::pair<float,int>> buffer;
+    if (buffer.size() < size) buffer.resize(size);
+    return buffer;
+
 }
 }
 
-void iqk_grouped_top_k([[maybe_unused]] ggml_tensor * dst, [[maybe_unused]] int ith, [[maybe_unused]] int nth) {
+void iqk_grouped_top_k(ggml_tensor * dst, int ith, int nth) {
     auto src = dst->src[0];
     GGML_ASSERT(dst->type == GGML_TYPE_F32);
     GGML_ASSERT(src->type == GGML_TYPE_F32);
@@ -42,15 +51,12 @@ void iqk_grouped_top_k([[maybe_unused]] ggml_tensor * dst, [[maybe_unused]] int 
     int n_top_groups = dst->op_params[1];
     int nk           = dst->op_params[2];
 
-    //if (ith == 0) printf("%s: ne00 = %ld, n_groups = %d, n_top_groups = %d, nk = %d\n", __func__, src->ne[0], n_groups, n_top_groups, nk);
-
     int ne00 = src->ne[0];
     GGML_ASSERT(ne00%n_groups == 0);
     int n_per_group = ne00/n_groups;
     GGML_ASSERT(nk <= n_per_group);
 
-    thread_local std::vector<std::pair<float,int>> aux;
-    if ((int)aux.size() < n_per_group + n_groups) aux.resize(n_per_group + n_groups);
+    auto& aux = get_work_buffer(n_per_group + n_groups);
 
     auto groups = aux.data() + n_per_group;
 
@@ -59,8 +65,8 @@ void iqk_grouped_top_k([[maybe_unused]] ggml_tensor * dst, [[maybe_unused]] int 
         auto result = (float *)((char *)dst->data + ir*dst->nb[1]);
         for (int ig = 0; ig < n_groups; ++ig) {
             //groups[ig] = { group_score(n_per_group, data + ig*n_per_group), ig };
-            groups[ig] = { group_score(n_per_group, nk, data + ig*n_per_group, (float *)aux.data()), ig };
             //groups[ig] = { group_score_max(n_per_group, data + ig*n_per_group), ig };
+            groups[ig] = { group_score(n_per_group, nk, data + ig*n_per_group, (float *)aux.data()), ig };
         }
         std::partial_sort(groups, groups + n_top_groups, groups + n_groups, std::greater<std::pair<float,int>>{});
 
@@ -72,64 +78,6 @@ void iqk_grouped_top_k([[maybe_unused]] ggml_tensor * dst, [[maybe_unused]] int 
             int jg = groups[ig].second;
             for (int j = 0; j < n_per_group; ++j) result[jg*n_per_group + j] = -INFINITY;
         }
-
-    }
-
-}
-
-void iqk_grouped_top_k_orig([[maybe_unused]] ggml_tensor * dst, [[maybe_unused]] int ith, [[maybe_unused]] int nth) {
-    auto src = dst->src[0];
-    GGML_ASSERT(dst->type == GGML_TYPE_F32);
-    GGML_ASSERT(src->type == GGML_TYPE_F32);
-    GGML_ASSERT(ggml_are_same_shape(src, dst));
-
-    auto nrows = ggml_nrows(src);
-    auto npt   = (nrows + nth - 1)/nth;
-    auto first = npt*ith;
-    auto last  = std::min(first + npt, nrows);
-    if (last <= first) return;
-
-    int n_groups     = dst->op_params[0];
-    int n_top_groups = dst->op_params[1];
-    int nk           = dst->op_params[2];
-
-    //if (ith == 0) printf("%s: ne00 = %ld, n_groups = %d, n_top_groups = %d, nk = %d\n", __func__, src->ne[0], n_groups, n_top_groups, nk);
-
-    int ne00 = src->ne[0];
-    GGML_ASSERT(ne00%n_groups == 0);
-    int n_per_group = ne00/n_groups;
-    GGML_ASSERT(nk <= n_per_group);
-
-    thread_local std::vector<std::pair<float,int>> aux;
-    if ((int)aux.size() < n_per_group + n_groups) aux.resize(n_per_group + n_groups);
-
-    auto groups = aux.data() + n_per_group;
-
-    for (int ir = first; ir < last; ++ir) {
-        auto data = (const float *)((const char *)src->data + ir*src->nb[1]);
-        auto result = (float *)((char *)dst->data + ir*dst->nb[1]);
-        for (int j = 0; j < ne00; ++j) result[j] = -INFINITY;
-        for (int ig = 0; ig < n_groups; ++ig) {
-            for (int j = 0; j < n_per_group; ++j) {
-                int jj = ig*n_per_group + j;
-                aux[j] = { data[jj], jj };
-            }
-            std::partial_sort(aux.begin(), aux.begin() + nk, aux.end(), std::greater<std::pair<float,int>>{});
-            for (int j = 0; j < nk; ++j) result[aux[j].second] = data[aux[j].second];
-            //float sum = 0;
-            //for (int j = 0; j < nk; ++j) sum += aux[j].first;
-            //groups[ig] = { sum, ig };
-        }
-        //std::partial_sort(groups, groups + n_top_groups, groups + n_groups, std::greater<std::pair<float,int>>{});
-
-        //for (int ig = 0; ig < n_top_groups; ++ig) {
-        //    int jg = groups[ig].second;
-        //    for (int j = 0; j < n_per_group; ++j) result[jg*n_per_group + j] = data[jg*n_per_group + j];
-        //}
-        //for (int ig = n_top_groups; ig < n_groups; ++ig) {
-        //    int jg = groups[ig].second;
-        //    for (int j = 0; j < n_per_group; ++j) result[jg*n_per_group + j] = -INFINITY;
-        //}
 
     }
 
@@ -151,9 +99,7 @@ void iqk_argsort(ggml_tensor * dst, int ith, int nth) {
     int nk = dst->op_params[1];
 
     int ne00 = src->ne[0];
-    thread_local std::vector<std::pair<float,int>> aux;
-    if ((int)aux.size() < ne00) aux.resize(ne00);
-    //std::vector<std::pair<float,int>> aux(ne00);
+    auto& aux = get_work_buffer(ne00);
 
     for (int ir = first; ir < last; ++ir) {
         auto data = (const float *)((const char *)src->data + ir*src->nb[1]);
