747f411da5485e60d771608e6a0c794d02439994
Kawrakow <iwankawrakow@gmail.com>
2025-10-18 07:22:35 +0300

    Grouped expert routing (CUDA) (#838)
    * WIP
    
    * cuda: grouped top_k
    
    * This is very slightly better
    
    ---------
    
    Co-authored-by: Iwan Kawrakow <iwan.kawrakow@gmail.com>

--- ggml/src/ggml-cuda/sumrows.cuh
+++ ggml/src/ggml-cuda/sumrows.cuh
@@ -1,3 +1,5 @@
 #include "common.cuh"
 
 void ggml_cuda_op_sum_rows(ggml_backend_cuda_context & ctx, ggml_tensor * dst);
+
+void sum_rows_f32_cuda(const float * x, float * dst, const int ncols, const int nrows, cudaStream_t stream);
