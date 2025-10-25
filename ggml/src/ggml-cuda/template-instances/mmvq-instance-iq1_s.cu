a7d050426ad14efbd8214e78a31de494adba390b
Iwan Kawrakow <iwan.kawrakow@gmail.com>
2025-10-25 11:09:37 +0300

    Somehow I forgot to change the ggml_type in the legacy template calls

--- ggml/src/ggml-cuda/template-instances/mmvq-instance-iq1_s.cu
+++ ggml/src/ggml-cuda/template-instances/mmvq-instance-iq1_s.cu
@@ -1,6 +1,6 @@
 #include "../mmvq-templates.cuh"
 
 void mul_mat_vec_iq1_s_q8_1_cuda(const mmvq_args & args, cudaStream_t stream) {
-    mul_mat_vec_q_cuda<GGML_TYPE_Q4_0>(args, stream);
+    mul_mat_vec_q_cuda<GGML_TYPE_IQ1_S>(args, stream);
 }
 
