8f7dd2f06b6ae914f3439f3781654561e59b90e2
Kawrakow <iwankawrakow@gmail.com>
2025-11-20 09:11:01 +0000

    Make gguf-py stuff work with numpy 2.0

--- gguf-py/scripts/gguf_dump.py
+++ gguf-py/scripts/gguf_dump.py
@@ -21,11 +21,11 @@ logger = logging.getLogger("gguf-dump")
 
 
 def get_file_host_endian(reader: GGUFReader) -> tuple[str, str]:
-    host_endian = 'LITTLE' if np.uint32(1) == np.uint32(1).newbyteorder("<") else 'BIG'
+    file_endian = reader.endianess.name
     if reader.byte_order == 'S':
-        file_endian = 'BIG' if host_endian == 'LITTLE' else 'LITTLE'
+        host_endian = 'BIG' if file_endian == 'LITTLE' else 'LITTLE'
     else:
-        file_endian = host_endian
+        host_endian = file_endian
     return (host_endian, file_endian)
 
 
