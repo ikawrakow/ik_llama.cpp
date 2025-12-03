ab19054a798b4c75c977fa91253e002364450bc3
Kawrakow <iwankawrakow@gmail.com>
2025-12-03 10:51:32 +0000

    Use standard attention for Ministral3
    Required adding the "temperature scaling" to the standard attention
    implementation.
    
    But in this way split mode "graph" is automatically supported.

--- src/llama.cpp
+++ src/llama.cpp
@@ -1728,6 +1728,7 @@ static bool is_model_split_supported(const llama_model & model) {
         LLM_ARCH_LLAMA,
         LLM_ARCH_QWEN3MOE,
         LLM_ARCH_GLM4_MOE,
+        LLM_ARCH_MISTRAL3,
     };
     auto it =  k_supported.find(model.arch);
     return it != k_supported.end();
