90f36eb517cbe7af5b4b3348f567aaaca760d2c8
Kawrakow <iwankawrakow@gmail.com>
2025-12-03 13:43:31 +0100

    Use standard attention for Ministral3 (#1032)
    Required adding the "temperature scaling" to the standard attention
    implementation.
    
    But in this way split mode "graph" is automatically supported.
    
    Co-authored-by: Iwan Kawrakow <iwan.kawrakow@gmail.com>

--- src/llama-build-context.h
+++ src/llama-build-context.h
@@ -407,6 +407,6 @@ llm_expert_gating_func_type   gating_op,
     static ggml_cgraph * llama_build_graph(llama_context & lctx, const llama_batch & batch, bool worst_case);
 
     ggml_tensor * build_std_attention(ggml_cgraph * gf, ggml_tensor * cur, ggml_tensor * inp_pos, ggml_tensor * rope_factors,
-            ggml_tensor * KQ_mask, ggml_tensor * sinks, float KQ_scale, float f_attn_scale, int n_swa, int il);
+            ggml_tensor * KQ_mask, ggml_tensor * sinks, ggml_tensor * inp_attn_scale, float KQ_scale, float f_attn_scale, int n_swa, int il);
 
 };
