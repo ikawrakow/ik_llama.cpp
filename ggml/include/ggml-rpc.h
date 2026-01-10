c03ee1a4d297840ea91ee9ffde6e402a9e993b42
firecoperana <xuqiaowei1124@gmail.com>
2026-01-10 00:01:22 -0600

    server: improve speed of speculative decoding (#1119)
    * server: improve speed of speculative decoding
    
    change logs
    
    rpc: add recompute
    
    spec dec fix
    
    * Fix n_batch_size not set to context size for draft model
    
    ---------
    
    Co-authored-by: firecoperana <firecoperana>

--- ggml/include/ggml-rpc.h
+++ ggml/include/ggml-rpc.h
@@ -9,7 +9,7 @@ extern "C" {
 
 #define RPC_PROTO_MAJOR_VERSION    3
 #define RPC_PROTO_MINOR_VERSION    5
-#define RPC_PROTO_PATCH_VERSION    1
+#define RPC_PROTO_PATCH_VERSION    2
 #define GGML_RPC_MAX_SERVERS       16
 
 // backend API
