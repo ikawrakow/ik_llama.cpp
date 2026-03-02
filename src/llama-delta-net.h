56158ec20b9f77dcb273f31faac3dc4a73284e74
Kawrakow <iwankawrakow@gmail.com>
2026-03-02 16:27:18 +0000

    WIP

--- src/llama-delta-net.h
+++ src/llama-delta-net.h
@@ -40,4 +40,10 @@ private:
             ggml_tensor * ssm_beta_alpha, ggml_tensor * ssm_beta, ggml_tensor * ssm_alpha,
             ggml_tensor * ssm_dt, ggml_tensor * ssm_a, int64_t num_k_heads, int64_t num_v_heads, int64_t n_seqs,
             ggml_tensor * cur, int il, const llm_build_cb & cb, ggml_cgraph * gf);
+
+    static ggml_tensor * build_qkv(ggml_context * ctx0, ggml_tensor * state_storage, ggml_tensor * ssm_conv1d,
+            ggml_tensor * qkv_mixed, ggml_tensor * inp_s_seq_qnext, ggml_tensor * beta, ggml_tensor * gate,
+            int64_t head_k_dim, int64_t num_k_heads, int64_t head_v_dim, int64_t num_v_heads, int64_t ssm_d_conv,
+            int64_t state_seq_id_local, uint32_t qnext_state_slots, bool reset_state_local,
+            float eps_norm, int repeat_type, int il, const llm_build_cb & cb, ggml_cgraph * gf);
 };
