a9f1bfb7403d2e7875e8dbf1380855f6644c36c2
Kawrakow <iwankawrakow@gmail.com>
2026-07-23 07:57:59 +0000

    ds4_comp: ratio = 4 specialization
    Surprisingly small performance gain

--- ggml/src/ggml-cuda/ds4_comp.cu
+++ ggml/src/ggml-cuda/ds4_comp.cu
@@ -40,8 +40,45 @@ static __global__ void k_ds4_comp(int ne0, int nblock, int ratio, int nidx,
     dst[ib*ne0 + i0] = sum_num / sum_den;
 }
 
+static __global__ void k_ds4_comp_4(int ne0, int nblock, int nidx,
+        size_t state_stride, size_t score_stride,
+        const float * __restrict__ state, const float * __restrict__ score, const int * __restrict__ idx, float * dst) {
+
+    int ii = blockIdx.x * blockDim.x + threadIdx.x;
+    int ib = ii / ne0;
+    if (ib >= nblock) {
+        return;
+    }
+    int i0 = ii % ne0;
+
+    idx += 4*ib;
+
+    float values[16];
+    #pragma unroll
+    for (int ir = 0; ir < 4; ++ir) {
+        int row_p = idx[ir];
+        int row_c = idx[ir+nidx];
+        values[2*ir+0] = score[row_p*score_stride + i0];
+        values[2*ir+1] = score[row_c*score_stride + i0 + ne0];
+        values[2*ir+8] = state[row_p*state_stride + i0];
+        values[2*ir+9] = state[row_c*state_stride + i0 + ne0];
+    }
+    float max_v = values[0];
+    #pragma unroll
+    for (int ir = 1; ir < 8; ++ir) max_v = max(max_v, values[ir]);
+
+    float sum_num = 0.0f, sum_den = 0.0f;
+    #pragma unroll
+    for (int ir = 0; ir < 8; ++ir) {
+        float w = expf(values[ir] - max_v);
+        sum_den += w;
+        sum_num += w*values[ir+8];
+    }
+    dst[ib*ne0 + i0] = sum_num / sum_den;
+}
+
 void ggml_cuda_op_ds4_comp(ggml_backend_cuda_context & ctx, ggml_tensor * dst) {
-    constexpr int k_block_size = 256;
+    constexpr int k_block_size = 128;
     auto * state = dst->src[0];
     auto * score = dst->src[1];
     auto *   idx = dst->src[2];
@@ -63,8 +100,14 @@ void ggml_cuda_op_ds4_comp(ggml_backend_cuda_context & ctx, ggml_tensor * dst) {
     int nelem = ne0 * nblock;
     int nb = (nelem + k_block_size - 1)/k_block_size;
 
-    k_ds4_comp<<<nb, k_block_size, 0, ctx.stream()>>>(ne0, nblock, ratio, idx->ne[0]/2,
-            state->nb[1]/sizeof(float), score->nb[1]/sizeof(float),
-            (const float *)state->data, (const float *)score->data, (const int *)idx->data, (float *)dst->data);
+    if (ratio == 4) {
+        k_ds4_comp_4<<<nb, k_block_size, 0, ctx.stream()>>>(ne0, nblock, idx->ne[0]/2,
+                state->nb[1]/sizeof(float), score->nb[1]/sizeof(float),
+                (const float *)state->data, (const float *)score->data, (const int *)idx->data, (float *)dst->data);
+    } else {
+        k_ds4_comp<<<nb, k_block_size, 0, ctx.stream()>>>(ne0, nblock, ratio, idx->ne[0]/2,
+                state->nb[1]/sizeof(float), score->nb[1]/sizeof(float),
+                (const float *)state->data, (const float *)score->data, (const int *)idx->data, (float *)dst->data);
+    }
 
 }
