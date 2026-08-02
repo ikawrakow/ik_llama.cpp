a2e9e752546fcb1ff0016156cd2f67c00cdeadc3
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-08-02 16:12:28 +0200

    fix typos in comments and user-facing strings
    - ngram-map.cpp: 'Do we haven a existing' -> 'Do we have an existing'
      (introduced in 1cb7e1bf39d 'spec : add self speculative decoding,
      ngram and refactor (PR 1261)')
    - build_mamba.cpp: 'weigth' -> 'weight' (introduced in 8befd92ea5f
      'Refactor model compute graphs (PR 1651)')
    - gguf-split.cpp: 'one of splits have 0 tensors' -> 'one of the splits
      has 0 tensors' (introduced in 75b580db0a3 'split: allow
      --split-max-size option (PR 6343)')
    - gguf-split.cpp: 'merged from %d split' -> 'merged from %d splits'
      (introduced in 1b5523dc796 'gguf-split: split and merge gguf per
      batch of tensors (PR 6135)')
    - convert-llama2c-to-ggml.cpp: missing opening quote in the help line,
      '(default %s\\')' -> '(default '%s\\')' (introduced in bb9ebb43943
      'Adding support for llama2.c models (PR 2559)')

--- common/ngram-map.cpp
+++ common/ngram-map.cpp
@@ -416,7 +416,7 @@ void common_ngram_map_draft(common_ngram_map & map,
             continue;
         }
 
-        // Do we haven a existing value m-gram or a new one after the key at index i?
+        // Do we have an existing value m-gram or a new one after the key at index i?
         size_t idx_begin_value_key = i + n;
         int idx_value = -1;
         for (int v = 0; v < COMMON_NGRAM_MAX_VALUES; ++v) {
