6e7378f6167b120057137dc411bbd2590c5d7729
Kawrakow <iwankawrakow@gmail.com>
2026-08-13 15:25:28 +0200

    Another minor optimization on CUDA for split mode graph (#2298)
    * CUDA: fuse rms -> add -> rms
    
    * Another minor optimization on CUDA for split mode graph

--- ggml/src/ggml-cuda.cu
+++ ggml/src/ggml-cuda.cu
@@ -3934,7 +3934,7 @@ static bool ggml_cuda_compute_forward(ggml_backend_cuda_context & ctx, struct gg
                 i += 4;
             }
             else if (fusion && i + 2 < cgraph->n_nodes &&
-                cgraph->nodes[i+1]->op == GGML_OP_VIEW &&
+                (cgraph->nodes[i+1]->op == GGML_OP_VIEW || cgraph->nodes[i+1]->op == GGML_OP_RESHAPE) &&
                 cgraph->nodes[i+2]->op == GGML_OP_FUSED_RMS_NORM &&
                 dst->ne[2] == 1 && cgraph->nodes[i+2]->ne[2] == 1) {
                 ggml_cuda_op_fused_rms_rms_norm(ctx, dst, cgraph->nodes[i+2]);
