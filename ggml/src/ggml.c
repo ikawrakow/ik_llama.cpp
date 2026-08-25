2012d6f4d2a8b8506e001a1108e4c66b468c983d
Joel Farthing <farthing@me.com>
2026-08-25 02:59:20 -0500

    ggml: fix HC_POST single-token CPU chunk count

--- ggml/src/ggml.c
+++ ggml/src/ggml.c
@@ -24471,7 +24471,8 @@ static void ggml_compute_forward_hc_post_f32(
         const float * post_r = (const float *)((const char *)post->data);
         const float * comb_r = (const float *)((const char *)comb->data);
 
-        int nchunk = (ne0 + nth - 1)/nth;
+        // chunks are 64 elements wide (see `first` below); threads stride over chunks
+        int nchunk = (ne0 + 63)/64;
 
         for (int ic = ith; ic < nchunk; ic += nth) {
             int first = 64*ic;
