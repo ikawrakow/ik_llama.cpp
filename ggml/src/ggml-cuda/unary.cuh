06825c7cc56eab2aff624fa48ac2a0dc0c2e4963
Kawrakow <iwankawrakow@gmail.com>
2026-08-28 15:52:11 +0000

    Fuse scale and sigmoid/silu on CUDA

--- ggml/src/ggml-cuda/unary.cuh
+++ ggml/src/ggml-cuda/unary.cuh
@@ -101,3 +101,5 @@ void ggml_cuda_op_multi_add(ggml_backend_cuda_context & ctx, ggml_tensor * dst);
 void ggml_cuda_fused_softplus(ggml_backend_cuda_context & ctx, ggml_tensor * dst);
 
 void ggml_cuda_fused_mul_exp_mul(ggml_backend_cuda_context & ctx, ggml_tensor * dst);
+
+bool ggml_cuda_op_scale_unary(ggml_backend_cuda_context & ctx, ggml_tensor * dst);
