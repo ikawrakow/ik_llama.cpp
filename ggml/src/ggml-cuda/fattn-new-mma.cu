04c55501e702a7c4ea4bd5e676784736d1890335
Kawrakow <iwankawrakow@gmail.com>
2026-08-28 07:25:13 +0000

    iCUDA: handle GQA = 12 for head size = 256 via new MMA

--- ggml/src/ggml-cuda/fattn-new-mma.cu
+++ ggml/src/ggml-cuda/fattn-new-mma.cu
@@ -2277,8 +2277,12 @@ void ggml_cuda_flash_attn_ext_mma_new(ggml_backend_cuda_context & ctx, ggml_tens
     }
     if (K->ne[0] == 256) {
         GGML_ASSERT(Q->ne[0] == 256 && V->ne[0] == 256);
-        if (gqa_ratio == 6) {
+        if (gqa_ratio % 6 == 0) {
             ggml_cuda_flash_attn_ext_mma_f16_case<256, 256, 1, 8>(ctx, dst);
+        //if (gqa_ratio == 12) {
+        //    ggml_cuda_flash_attn_ext_mma_f16_case<256, 256, 1, 16>(ctx, dst);
+        //} else if (gqa_ratio == 6) {
+        //    ggml_cuda_flash_attn_ext_mma_f16_case<256, 256, 1, 8>(ctx, dst);
         } else {
             GGML_ABORT("Not implemented");
         }
