f529fe5eff61008c2b13ca29cf6e700af541c4ee
banana split <hyunsoo.david.kim@gmail.com>
2026-08-31 22:12:04 -0400

    restore draft collection

--- common/sampling.cpp
+++ common/sampling.cpp
@@ -843,6 +843,7 @@ std::vector<llama_token> common_sampler_sample_and_accept_n(
 
     std::uniform_real_distribution<float> uniform(0.0f, 1.0f);
     const auto emit = [&](llama_token id) {
+        gsmpl->playing_text += common_token_to_piece(ctx, id, true);
         common_sampler_accept(gsmpl, ctx, id, true);
         result.push_back(id);
     };
