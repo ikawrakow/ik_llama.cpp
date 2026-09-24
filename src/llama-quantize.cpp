dad2cb3e55138cbdd7df988c66c0c3c54f6d34ad
Kawrakow <iwankawrakow@gmail.com>
2026-09-24 10:17:42 +0200

    Disable using IQ4_KS_R16 as type override (#2526)

--- src/llama-quantize.cpp
+++ src/llama-quantize.cpp
@@ -282,7 +282,9 @@ static ggml_type change_type_if_necessary(ggml_type new_type, int nx, int ny) {
             case GGML_TYPE_IQ4_XS_R8:
             case GGML_TYPE_IQ3_KT:
             case GGML_TYPE_IQ4_KT:
-            case GGML_TYPE_IQ4_XS: new_type = ny % 16 == 0 ? GGML_TYPE_IQ4_KS_R16 : GGML_TYPE_IQ4_NL; break;
+            // Disable GGML_TYPE_IQ4_KS_R16 until we have CUDA implementation for it
+            //case GGML_TYPE_IQ4_XS: new_type = ny % 16 == 0 ? GGML_TYPE_IQ4_KS_R16 : GGML_TYPE_IQ4_NL; break;
+            case GGML_TYPE_IQ4_XS: new_type = GGML_TYPE_IQ4_NL; break;
             case GGML_TYPE_IQ4_K:
             case GGML_TYPE_IQ4_K_R4:
             case GGML_TYPE_Q4_K_R4:
